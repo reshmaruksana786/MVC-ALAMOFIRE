4e2f7ca2090a372578e5fa37501d9122e5c1c291
reshma <reshmaruksana786786@gmail.com>
2020-01-20 16:20:29 +0530

    Initial Commit

---
+++ MVC&ALAMOFIRE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVC&ALAMOFIRE
+//
+//  Created by Syed.Reshma Ruksana on 20/01/20.
+//  Copyright © 2020 Syed.Reshma Ruksana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
